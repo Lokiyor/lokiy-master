3f1b1c48570798417b992ecb9c42630aec513744
lujingyao <ljy88808@gmail.com>
2019-07-15 20:04:38 +0800

    common子模块建立/gateway模块建立

---
+++ lokiy-gateway/src/main/java/com/lokiy/cloud/gateway/LokiyGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.lokiy.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lokiy
+ * @date 2019/7/15 19:54
+ * @description: gateway
+ */
+@SpringBootApplication
+public class LokiyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokiyGatewayApplication.class, args);
+    }
+
+}
