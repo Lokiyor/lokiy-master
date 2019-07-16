df0a9a358189cd8e17dcee795cd89f92b4487a5c
lujingyao <ljy88808@gmail.com>
2019-07-16 19:36:03 +0800

    auth模块建立/中间件模块建立

--- lokiy-gateway/src/main/java/com/lokiy/cloud/gateway/LokiyGatewayApplication.java
+++ lokiy-gateway/src/main/java/com/lokiy/cloud/gateway/LokiyGatewayApplication.java
@@ -1,14 +1,14 @@
 package com.lokiy.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @author Lokiy
  * @date 2019/7/15 19:54
  * @description: gateway
  */
-@SpringBootApplication
+@SpringCloudApplication
 public class LokiyGatewayApplication {
 
     public static void main(String[] args) {
---
+++ lokiy-modules/lokiy-auth/src/main/java/com/lokiy/cloud/auth/LokiyAuthApplication.java
@@ -0,0 +1,18 @@
+package com.lokiy.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lokiy
+ * @date 2019-07-16 11:06:00
+ * @description auth-module
+ */
+@SpringBootApplication
+public class LokiyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokiyAuthApplication.class, args);
+    }
+
+}
