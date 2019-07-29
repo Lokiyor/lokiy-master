a136afc93db8102988adab26ac8923733fd31fc0
lujingyao <ljy88808@gmail.com>
2019-07-29 19:33:13 +0800

    mp配置

--- lokiy-common/lokiy-common-config/src/main/java/com/lokiy/cloud/common/config/mp/MyBatiesPlusConfig.java
+++ lokiy-common/lokiy-common-config/src/main/java/com/lokiy/cloud/common/config/mp/MyBatiesPlusConfig.java
@@ -1,8 +1,14 @@
 package com.lokiy.cloud.common.config.mp;
 
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -15,6 +21,38 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan
 @Slf4j
 public class MyBatiesPlusConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 逻辑删除
+     *
+     * @return
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+    /**
+     *  sql执行效率插件
+     * @return
+     */
+    @Bean
+    @Profile({"dev","beta"})
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(1000);
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+
 
 
 }
