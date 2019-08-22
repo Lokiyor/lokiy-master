9fedb65918574752f8d180176c8cdf597ccec666
lujingyao <ljy88808@gmail.com>
2019-08-22 09:39:04 +0800

    短信模块

---
+++ lokiy-thd/lokiy-sms/src/main/java/com/lokiy/cloud/thd/sms/LokiySmsApplication.java
@@ -0,0 +1,13 @@
+package com.lokiy.cloud.thd.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokiySmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokiySmsApplication.class, args);
+    }
+
+}
